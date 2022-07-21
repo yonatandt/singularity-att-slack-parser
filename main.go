0f62c61b53a27ee67b9cde7e31e65b11bba2b720
Yonatan Tal <yonatald@gmail.com>
2022-07-21 11:23:52 +0300

    refactor & add postback update type

--- main.go
+++ main.go
@@ -45,14 +45,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 		switch message.Type {
 		case "deploy":
-			// deploy := message.ToDeploy()
-			// fmt.Println("Deploy: ", deploy) // for debugging
-			// structuredMessage = deploy.structure()
-			// fmt.Printf("DeployStructured: %+v\n", structuredMessage) // for debugging
-			structuredMessage = message.ToDeploy2()
+			structuredMessage = message.ToDeploy()
 		case "ff-change":
 			structuredMessage = message.ToFeatureFlagChange()
-			fmt.Println("FeatureFlagChange: ", structuredMessage) // for debugging
+		case "postback-update":
+			structuredMessage = message.ToPostbackUpdate()
 		default:
 			fmt.Println("Unknown message type: ", message.Type)
 			return
@@ -88,34 +85,16 @@ type Message struct {
 	Components    string `json:"affected_services,omitempty"`
 	WithMigration string `json:"includes_migration,omitempty"`
 	// Feature Flag Change Related Fields:
-	FFChangeType   string `json:"change_type,omitempty"`
-	FeatureName    string `json:"feature_name,omitempty"`
-	IsForAllOrgs   string `json:"is_for_all_orgs,omitempty"`
+	FFChangeType string `json:"change_type,omitempty"`
+	FeatureName  string `json:"feature_name,omitempty"`
+	IsForAllOrgs string `json:"is_for_all_orgs,omitempty"`
+	// Postback Update & Feature Flag Change Related Fields:
 	Orgs           string `json:"orgs,omitempty"`
 	AdditionalInfo string `json:"additional_info,omitempty"`
 	Query          string `json:"query,omitempty"`
-}
-
-type Deploy struct {
-	ID            string   `json:"id"`
-	Type          string   `json:"event_type"`
-	Subtype       string   `json:"subtype"`
-	Description   string   `json:"header"`
-	RepoName      string   `json:"repo_name",omitempty`
-	PullRequest   string   `json:"pull_request,omitempty"`
-	Components    []string `json:"affected_services,omitempty"`
-	WithMigration bool     `json:"migration_files,omitempty"`
-	Timestamps    string   `json:"timestamps,omitempty"`
-}
-
-type FeatureFlagChange struct {
-	ID              string           `json:"id"`
-	Type            string           `json:"event_type"`
-	Subtype         string           `json:"subtype"`
-	Description     string           `json:"header"`
-	Subject         Subject          `json:"subject"`
-	RelatedSubjects []RelatedSubject `json:"related_subjects"`
-	Timestamps      string           `json:"timestamps"`
+	// Postback Update Related Fields:
+	PostbackUpdateType string `json:"postback_update_type,omitempty"`
+	AdNetwork          string `json:"ad_net,omitempty"`
 }
 
 type DeployStructured struct {
@@ -161,48 +140,7 @@ func parseJSONToMessage(jsonText string) Message {
 	return message
 }
 
-func (m *Message) ToDeploy() Deploy {
-	var deploy Deploy
-	deploy.ID = uuid.NewV4().String()
-	deploy.Type = m.Type
-	deploy.Subtype = "merge"
-	deploy.Description = m.Description
-	deploy.PullRequest = removeFirstCharAndLastChar(m.PullRequest)
-	deploy.RepoName = getRepoNameFromGithubPrURL(deploy.PullRequest)
-	deploy.WithMigration = translateToBoolean(m.WithMigration)
-	// split by , and remove spaces:
-	if m.Components != "" {
-		deploy.Components = strings.Split(m.Components, ",")
-	}
-	deploy.Timestamps = time.Now().Format(time.RFC3339)
-	return deploy
-}
-
-func (deploy *Deploy) structure() StructuredMessage {
-	var deployStructured StructuredMessage
-	deployStructured.ID = deploy.ID
-	deployStructured.Type = deploy.Type
-	deployStructured.Subtype = deploy.Subtype
-	deployStructured.Description = deploy.Description
-	deployStructured.Timestamps = deploy.Timestamps
-	// create subject:
-	deployStructured.Subject = Subject{Type: "repo", Name: deploy.RepoName}
-	// create related subjects:
-	relatedSubjects := make([]RelatedSubject, 0)
-	if deploy.PullRequest != "" {
-		relatedSubject := RelatedSubject{Type: "pull_request", StringValue: deploy.PullRequest}
-		relatedSubjects = append(relatedSubjects, relatedSubject)
-	}
-	if len(deploy.Components) > 0 {
-		relatedSubject := RelatedSubject{Type: "components", ArrayValue: deploy.Components}
-		relatedSubjects = append(relatedSubjects, relatedSubject)
-	}
-	relatedSubjects = append(relatedSubjects, RelatedSubject{Type: "has_migration", StringValue: strconv.FormatBool(deploy.WithMigration)}) //TODO: fix this!
-	deployStructured.RelatedSubjects = relatedSubjects
-	return deployStructured
-}
-
-func (m *Message) ToDeploy2() StructuredMessage {
+func (m *Message) ToDeploy() StructuredMessage {
 	var deploy StructuredMessage
 	deploy.ID = uuid.NewV4().String()
 	deploy.Type = m.Type
@@ -274,6 +212,61 @@ func (m *Message) BuildFFChangeDescription() string {
 		description = "Feature Flag " + m.FeatureName + " has been " + action + " for orgs: " + m.Orgs
 	}
 	return description
+}
+
+func (m *Message) ToPostbackUpdate() StructuredMessage {
+	var postbackUpdate StructuredMessage
+	postbackUpdate.ID = uuid.NewV4().String()
+	postbackUpdate.Type = m.Type
+	postbackUpdate.Subtype = m.PostbackUpdateType
+	postbackUpdate.Timestamps = time.Now().Format(time.RFC3339)
+
+	table_name := "postback_" + strings.ToLower(m.PostbackUpdateType)
+
+	relatedSubjects := []RelatedSubject{
+		{Type: "query", StringValue: strings.ReplaceAll(m.Query, "\n", " ")},
+	}
+
+	orgs_array := []string{}
+	ad_net := m.AdNetwork
+	subjectDescription := ""
+
+	if m.Orgs != "" {
+		orgs_array = strings.Split(m.Orgs, ",")
+		relatedSubject := RelatedSubject{Type: "orgs", ArrayValue: orgs_array}
+		relatedSubjects = append(relatedSubjects, relatedSubject)
+	}
+
+	if m.AdNetwork != "" {
+		relatedSubjects = append(relatedSubjects, RelatedSubject{Type: "ad_network", StringValue: m.AdNetwork})
+	}
+
+	if m.AdditionalInfo != "" {
+		relatedSubjects = append(relatedSubjects, RelatedSubject{Type: "additional_info", StringValue: m.AdditionalInfo})
+	}
+
+	if len(orgs_array) == 0 && ad_net == "" {
+		postbackUpdate.Subject = Subject{Type: "table", Name: table_name}
+	} else {
+		relatedSubjects = append(relatedSubjects, RelatedSubject{Type: "table", StringValue: table_name})
+		if len(orgs_array) == 0 {
+			postbackUpdate.Subject = Subject{Type: "Ad Network", Name: ad_net}
+			subjectDescription = " for Ad Network " + ad_net
+		} else {
+			if len(orgs_array) == 1 {
+				postbackUpdate.Subject = Subject{Type: "org", Name: orgs_array[0]}
+				subjectDescription = " for The Organization " + orgs_array[0]
+			} else {
+				postbackUpdate.Subject = Subject{Type: "orgs", Name: strings.Join(orgs_array, ", ")}
+				subjectDescription = " for The Organizations " + strings.Join(orgs_array, ", ")
+			}
+		}
+	}
+
+	postbackUpdate.RelatedSubjects = relatedSubjects
+	postbackUpdate.Description = "Updating Postback " + m.PostbackUpdateType + subjectDescription + "."
+
+	return postbackUpdate
 }
 
 func write_structured_json_to_file(sm *StructuredMessage, filepath string) {
